075b5090ee545da3679ab26b93c849cf8c0dbf08
Sarcoma <sarcoma@live.co.uk>
2021-05-23 06:34:19 +0100

    Add router basepath

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import RadioView from './pages/radio-view/radio-view';
 import './app.module.css';
+import { publicUrl } from './constants';
 
 function App() {
     return (
-        <BrowserRouter>
+        <BrowserRouter basename={publicUrl}>
             <Switch>
                 <Route exact path="/" component={RadioView} />
                 <Route path="/tz/:tz" component={RadioView} />
