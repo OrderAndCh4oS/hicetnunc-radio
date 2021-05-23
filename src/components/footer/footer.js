3809791d9d01dabfe25d081070bc5e24b50722cc
Joshua Clayton <joshua.clayton@nyu.edu>
2021-05-23 12:07:16 -0400

    Add to footer and credits

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -3,13 +3,17 @@ import styles from './styles.module.css';
 const Footer = () =>
     <div className={styles.footerBar}>
         <h2 className={styles.footerTitle}>Created as part of Hicathon Hackday 2021</h2>
+
+        <p className={styles.footerText}><a href='https://github.com/OrderAndCh4oS/hicetnunc-radio'>GitHub Repository</a></p>
+
         <p className={styles.footerText}>Working Group:{' '}
             <a href='https://twitter.com/lauzaki'>@lauzaki</a>,{' '}
             <a href='https://twitter.com/andreasrau_eu'>@andreasrau_eu</a>,{' '}
             <a href='https://twitter.com/uvdsc'>@uvdsc</a>,{' '}
             <a href='https://twitter.com/__orderandchaos'>@__orderandchaos</a>,{' '}
             <a href='https://twitter.com/webidente'>@webidente</a>,{' '}
-            <a href='https://twitter.com/BabyCommando_'>@BabyCommando_</a>
+            <a href='https://twitter.com/BabyCommando_'>@BabyCommando_</a>,{' '}
+            <a href='https://twitter.com/jclayton'>@jclayton</a>
         </p>
     </div>
 ;
