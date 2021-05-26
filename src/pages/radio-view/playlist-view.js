880a6f99a9e69128bd8f970d6657b33a5e1c5a9d
babycommando <joao@trackers.cx>
2021-05-26 10:23:40 +0000

    add link

--- src/pages/radio-view/playlist-view.js
+++ src/pages/radio-view/playlist-view.js
@@ -21,8 +21,105 @@ const PlaylistView = () => {
                 </select>
             </div>
             <PlaylistPlayer playlist={selectedPlaylist}/>
+
+
+    {/* Here starts the squared More Playlists component*/}
+
+         {/* Wraps the whole component and define it's margins*/}
+        <div className={styles.MorePlaylists}>
+
+            {/*Title*/}
+            <h4 className={styles.morePlaylistsTitle}>More Playlists</h4>
+
+                {/*Defines the grid rules*/}
+                <div className={styles.boxPlaylists}>
+
+                    {/* 1/7 individual block of the gid*/}
+                    <div className={styles.boxPlaylists1}>
+                        <div className={styles.boxImage}>
+                            <a href="https://www.hicetnunc.xyz/">
+                                <img src="https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/modern-art-square-edward-fielding.jpg" 
+                                className={styles.boxImage2}></img>
+                                <p className={styles.boxText1}>whatifthenameiswaytoolongfortheboxandhasnospaces</p>
+                            </a>
+                        </div>
+                    </div>
+
+                    {/* 2/7 individual block of the gid*/}
+                    <div className={styles.boxPlaylists2}>
+                        <div className={styles.boxImage}>
+                            <a href="https://www.hicetnunc.xyz/">
+                                <img src="https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/modern-art-square-edward-fielding.jpg" 
+                                className={styles.boxImage2}></img>
+                                <p className={styles.boxText1}>Playlist</p>
+                            </a>
+                        </div>
+                    </div>
+
+                    {/* 3/7 individual block of the gid*/}
+                    <div className={styles.boxPlaylists3}>
+                        <div className={styles.boxImage}>
+                            <a href="https://www.hicetnunc.xyz/">
+                                <img src="https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/modern-art-square-edward-fielding.jpg" 
+                                className={styles.boxImage2}></img>
+                                <p className={styles.boxText1}>Playlist</p>
+                            </a>
+                        </div>
+                    </div>
+
+                    {/* 4/7 individual block of the gid*/}
+                    <div className={styles.boxPlaylists4}>
+                        <div className={styles.boxImage}>
+                            <a href="https://www.hicetnunc.xyz/">
+                                <img src="https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/modern-art-square-edward-fielding.jpg" 
+                                className={styles.boxImage2}></img>
+                                <p className={styles.boxText1}>Playlist</p>
+                            </a>
+                        </div>
+                    </div>
+
+                    {/* 5/7 individual block of the gid*/}
+                    <div className={styles.boxPlaylists5}>
+                        <div className={styles.boxImage}>
+                            <a href="https://www.hicetnunc.xyz/">
+                                <img src="https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/modern-art-square-edward-fielding.jpg" 
+                                className={styles.boxImage2}></img>
+                                <p className={styles.boxText1}>Playlist</p>
+                            </a>
+                        </div>
+                    </div>
+
+                    {/* 6/7 individual block of the gid*/}
+                    <div className={styles.boxPlaylists6}>
+                        <div className={styles.boxImage}>
+                            <a href="https://www.hicetnunc.xyz/">
+                                <img src="https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/modern-art-square-edward-fielding.jpg" 
+                                className={styles.boxImage2}></img>
+                                <p className={styles.boxText1}>Playlist</p>
+                            </a>
+                        </div>
+                    </div>
+
+                    {/* 7/7 individual block of the gid*/}
+                    <div className={styles.boxPlaylists7}>
+                        <div className={styles.boxImage}>
+                            <a href="https://www.hicetnunc.xyz/">
+                                <img src="https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/modern-art-square-edward-fielding.jpg" 
+                                className={styles.boxImage2}></img>
+                                <p className={styles.boxText1}>Playlist</p>
+                            </a>
+                        </div>
+                    </div>
+
+                </div>
+        </div>   
+         {/*Here it ends
+         but the dreams will never fade*/} 
+         
         </>
     );
 };
 
 export default PlaylistView;
+
+
