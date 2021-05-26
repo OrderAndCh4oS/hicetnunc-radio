735e883c1c38912e58e8db8ae67f4dba11b46ef2
Sarcoma <sarcoma@live.co.uk>
2021-05-26 20:31:03 +0100

    hash router

--- src/playlists/playlists.js
+++ src/playlists/playlists.js
@@ -9,7 +9,6 @@ export const playlists = [
                 src: `https://cloudflare-ipfs.com/ipfs/QmawLCNM666CsHajN7MfJH9baWq2JRshxXX7QETL9dR4NA`,
                 mimeType: 'audio/ogg',
                 creator: 'tz2EaL8HTqjZhVAWrQ3Ddp4wNCxPeE8CtM5f',
-                tags: ['music', 'psychedelic', 'hip hop', 'objkt4objkt']
             },
             {
                 id: 5302,
@@ -17,7 +16,6 @@ export const playlists = [
                 src: `https://cloudflare-ipfs.com/ipfs/Qmd8vTe22AdcYV84hTD2UEZFDicZZNf4j9bUBfDQsidy11`,
                 mimeType: 'audio/mpeg',
                 creator: 'tz2EaL8HTqjZhVAWrQ3Ddp4wNCxPeE8CtM5f',
-                tags: ['music', 'dub']
             },
             {
                 id: 5298,
@@ -25,7 +23,6 @@ export const playlists = [
                 src: `https://cloudflare-ipfs.com/ipfs/QmNNrraeevcUhDT4saUA6W1AqiukZvvyRtroCf57PhjjyV`,
                 mimeType: 'audio/mpeg',
                 creator: 'tz2EaL8HTqjZhVAWrQ3Ddp4wNCxPeE8CtM5f',
-                tags: ['music', 'cypherpunk']
             }
         ],
     },
