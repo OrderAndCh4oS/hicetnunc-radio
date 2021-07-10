e5aefe64142e46ebe4510d9f7e8f9de1c58450e0
Lauzaki <L@L.local>
2021-07-10 22:12:20 +0100

    draft playlist link

--- src/pages/radio-view/playlist-view.js
+++ src/pages/radio-view/playlist-view.js
@@ -5,11 +5,50 @@ import PlaylistTracks from '../../components/radio-player/playlist-tracks';
 import Playlists from '../../components/playlists/playlists';
 import CurrentPlaylist from '../../components/current-playlist/current-playlist';
 import useUserPlaylists from '../../hooks/use-user-playlists';
+import { useLocation } from "react-router-dom";
+import { gql, request } from 'graphql-request';
+import usePlaylist from '../../hooks/use-playlist';
+import { ipfsUrls } from '../../constants';
 
 const PlaylistView = () => {
     useTitle(`H=N Radio Playlists`);
-    const {userPlaylists} = useUserPlaylists();
 
+    const search = useLocation().search;
+    const objktIdsStr = new URLSearchParams(search).get("objktIds");
+    const objktIds = objktIdsStr?.split(',');
+    const {setTracks} = usePlaylist();
+
+   // if (objktIds) {
+       const objktList = objktIds.map((obj) => (`{id: {_eq: ${obj}}}`)).join(',')
+        const query = gql`
+            query AudioObjktData {
+                hic_et_nunc_token(where: {_or: [${objktList}]}) {
+                    id
+                    artifact_uri
+                    creator_id
+                    description
+                    display_uri
+                  }
+            }
+`;
+   // }
+
+            useEffect(() => {
+            (async () => {
+                const data = await request('https://api.hicdex.com/v1/graphql', query);
+                setTracks(data?.hic_et_nunc_token?.map(o => ({
+                    id: o.id,
+                    creator: o.creator_id,
+                    name: o.title,
+                    src: `${ipfsUrls[~~(Math.random() * ipfsUrls.length)]}/${o.artifact_uri.slice(7)}`,
+                    mimeType: o.mime,
+                    displayUri: o.display_uri,
+                })));
+            })();
+            // eslint-disable-next-line react-hooks/exhaustive-deps
+        }, []);
+
+    const { userPlaylists } = useUserPlaylists();
     const [playlists, setPlaylists] = useState(initialPlaylists);
     const [selectedPlaylist, setSelectedPlaylist] = useState(playlists[0]);
 
@@ -21,7 +60,7 @@ const PlaylistView = () => {
     useEffect(() => {
         setSelectedPlaylist(prevState => {
             const sp = playlists.find(np => prevState.name === np.name) || playlists[0];
-            return {...sp, forceUpdate: (sp?.forceUpdate || 0) + 1}; // Todo: Remove the need for forceUpdate
+            return { ...sp, forceUpdate: (sp?.forceUpdate || 0) + 1 }; // Todo: Remove the need for forceUpdate
         });
     }, [playlists]);
 
@@ -29,8 +68,8 @@ const PlaylistView = () => {
 
     return (
         <>
-            <CurrentPlaylist playlist={selectedPlaylist}/>
-            <PlaylistTracks playlist={selectedPlaylist}/>
+            <CurrentPlaylist playlist={selectedPlaylist} />
+            <PlaylistTracks playlist={selectedPlaylist} />
             <Playlists
                 handlePlaylistChange={handlePlaylistChange}
                 playlists={playlists}
